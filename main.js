42b94d081264d059f8e371b76697bb18677e6e29
jazzyclimber <jazzyclimber@gmail.com>
2019-02-24 21:33:18 -0600

    Changed Opacity function. Cleaned up some redundancy. Added addBlack()

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 //Set global Variables
 const container = document.querySelector('div#container');
-let defaultSize = 16;
+let defaultSize = 20;
+
+//Adds a 10% shade of black on each mousover
+function addBlack(e) {
+  let cell = e.target;
+  let data = parseFloat(cell.getAttribute('data-pass'));
+  if (data > 10) {
+    return
+  } else {
+    cell.setAttribute('data-pass', data + 1);
+  }
+cell.setAttribute('style', `background-color: rgba(0,0,0,${data / 10});`);
+};
 
 //creates boxes; adds class, adds listener, appends to container
 function buildGrid(squareSize) {
@@ -8,14 +20,15 @@ function buildGrid(squareSize) {
   for (let i = 1; i <= totalSquares; i++){
     let gridItem = document.createElement("div");
     gridItem.classList.add('grid-item');
-    gridItem.addEventListener('mouseover', e => e.target.classList.add('hover') )
+    gridItem.addEventListener('mouseover', addBlack )
+    gridItem.setAttribute('style', 'background-color: rgba(0,0,0,0);')
+    gridItem.setAttribute('data-pass', 1);
 
     container.appendChild(gridItem);
 
   }
   container.setAttribute("style", `grid-template-columns:repeat(${squareSize}, 1fr); grid-template-rows:repeat(${squareSize}, 1fr)`);
 };
-
 
 //Removes old grid appends with custom grid
 function userGrid() {
@@ -30,4 +43,5 @@ function userGrid() {
 const newGrid = document.querySelector('button#new-grid');
 newGrid.addEventListener('click', userGrid);
 
+//Call BuildGrid Function with default size
 buildGrid(defaultSize);
